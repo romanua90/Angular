18c80e27f0fa0704ef352f44cfbead1fea537c0d
Roman Stashchak <stashchak90@gmail.com>
2021-01-20 22:32:34 +0200

    les1 started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+import { AddressComponent } from './components/address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
