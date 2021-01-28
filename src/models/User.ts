f62e0115939291cadaa388446b0e8520816e5e23
Roman Stashchak <stashchak90@gmail.com>
2021-01-28 22:44:31 +0200

    les3 finished

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,18 +3,23 @@ export interface User {
   id: number;
   username: string;
   email: string;
-  address: string;
-  street: string;
-  suite: string;
-  city: string;
-  zipcode: number;
-  geo: string;
-  lat: number;
-  lng: number;
-  phone: number;
-  website:	string;
-  company: string;
-  name:	string;
-  catchPhrase: string;
-  bs: string;
+  address:
+    {
+      street: string; suite: string;
+      city: string;
+      zipcode: number;
+      geo: {
+        lat: number;
+        lng: number;
+      }
+    }
+  phone: string;
+  website: string;
+  company:
+    {
+      name: string;
+      catchPhrase: string;
+      bs: string;
+    }
+
 }
