90e3da93f9f4423578aaa55802a34c526ab2a392
Roman Stashchak <stashchak90@gmail.com>
2021-02-01 06:36:11 +0200

    les4 progress

---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Post} from "../../models/Post";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+posts: Post[];
+  constructor(private activatedRouter: ActivatedRoute) {
+    this.activatedRouter.data.subscribe(value => this.posts=value.postsData)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
